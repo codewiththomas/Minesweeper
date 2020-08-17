d7d4b87029056baf22be434c9568c5e3349dc92f
thomas.bazan@outlook.com <thomas.bazan@outlook.com>
2020-08-17 12:50:05 +0200

    Methode für den ersten Klick hinzugefügt

--- Minesweeper/Models/GameBoard.cs
+++ Minesweeper/Models/GameBoard.cs
@@ -45,5 +45,135 @@ namespace Minesweeper.Models
         {
             Initialize(Width, Height, MineCount);
         }
+
+
+
+        public List<Field> GetNeighborFields(int x, int y)
+        {
+            var currentField = Fields.Where(field => field.X == x && field.Y == y);
+
+            return GetFieldWithNeighbors(x, y).Except(currentField).ToList();
+        }
+
+
+
+        public List<Field> GetFieldWithNeighbors(int x, int y)
+        {
+            var nearbyFields = Fields.Where(field =>
+                field.X >= (x - 1)
+                && field.X <= (x + 1)
+                && field.Y >= (y - 1)
+                && field.Y <= (y + 1));
+            return nearbyFields.ToList();
+        }
+
+
+
+        public void FirstClick(int x, int y)
+        {
+            Random random = new Random();
+
+            var clickedFieldWithNeighbors = GetFieldWithNeighbors(x, y);
+
+            var availableFieldsForMines = Fields
+                .Except(clickedFieldWithNeighbors)
+                .OrderBy(param => random.Next());
+
+            var fieldsWithPlacedMines = availableFieldsForMines
+                .Take(MineCount)
+                .ToList()
+                .Select(p => new { p.X, p.Y });
+
+            foreach (var mine in fieldsWithPlacedMines)
+            {
+                Fields.Single(field =>
+                    field.X == mine.X
+                    && field.Y == mine.Y).IsMine = true;
+            }
+
+            foreach (var safeField in Fields.Where(field => !field.IsMine))
+            {
+                var nearbyFields = GetNeighborFields(safeField.X, safeField.Y);
+                safeField.AdjacentMines = nearbyFields.Count(param => param.IsMine);
+            }
+
+            GameState = GameState.InProgress;
+        }
+
+
+
+        public void Click(int x, int y)
+        {
+            if (GameState == GameState.NewGameStarted)
+            {
+                FirstClick(x, y);
+            }
+            RevealField(x, y);
+        }
+
+
+
+        public void RevealField(int x, int y)
+        {
+            var selectedField = Fields.First(field => field.X == x && field.Y == y);
+
+            selectedField.Reveal();
+
+            if (selectedField.IsMine)
+            {
+                GameState = GameState.Lost;
+                RevealAllMines();
+                return;
+            }
+
+            if (selectedField.AdjacentMines == 0)
+            {
+                RevealZeroes(x, y);
+            }
+
+            CompletionCheck();
+        }
+
+
+
+        private void RevealAllMines()
+        {
+            Fields
+                .Where(field => field.IsMine)
+                .ToList()
+                .ForEach(field => field.FieldState = FieldState.Revealed);
+        }
+
+
+
+        public void RevealZeroes(int x, int y)
+        {
+            var neighborFields = GetNeighborFields(x, y).Where(field => !field.IsRevealed);
+
+            foreach (var neighborField in neighborFields)
+            {
+                neighborField.FieldState = FieldState.Revealed;
+
+                if (neighborField.AdjacentMines == 0)
+                {
+                    RevealZeroes(neighborField.X, neighborField.Y);
+                }
+            }
+        }
+
+
+
+        private void CompletionCheck()
+        {
+            var coveredFields = Fields.Where(field => !field.IsRevealed).Select(field => field.Id);
+
+            var mineFields = Fields.Where(field => field.IsMine).Select(field => field.Id);
+
+            if (!coveredFields.Except(mineFields).Any())
+            {
+                GameState = GameState.Won;
+                //Stopwatch.Stop();
+            }
+        }
     }
 }
